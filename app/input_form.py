9034985521f9d13cb0de43512863f1bca0a9ad9b
Brogan <66849242+beawitcht@users.noreply.github.com>
2022-05-25 15:23:50 +0100

    add form validation + captcha

---
+++ app/input_form.py
@@ -0,0 +1,19 @@
+from lib2to3.pgen2.token import OP
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, SelectField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length, Email, Optional, AnyOf
+
+
+class InputForm(FlaskForm):
+    countries = SelectField("Country", choices=['England', 'Northern Ireland', 'Scotland', 'Wales'], validators=[
+        DataRequired(), AnyOf(['England', 'Northern Ireland', 'Scotland', 'Wales'], message="Please select a country")])
+    self_med = BooleanField("I am self medicating")
+    formal_diagnosis = BooleanField("I have a formal Diagnosis")
+    shared_care = BooleanField("I want a shared care agreement with my GP")
+    name = StringField("First Name", validators=[Optional()])
+    surname = StringField("Last Name", validators=[Optional()])
+    email = EmailField("Email", validators=[Optional()])
+    phone = StringField("Phone Number", validators=[Optional()])
+    docx = SubmitField("Generate Docx")
+    pdf = SubmitField("Generate PDF")
+    captcha = RecaptchaField()
--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, request, send_file
+from flask_wtf import FlaskForm
 from docproc.populate_doc import generate_document
+from input_form import InputForm
 import requests
 import os
 import json
 
+
 api_key = os.getenv('PDF_API_KEY')
 
 app = Flask(__name__)
+app.config['WTF_CSRF_ENABLED'] = False
+app.config['RECAPTCHA_PUBLIC_KEY'] = os.getenv('RECAPTCHA_PUBLIC_KEY')
+app.config['RECAPTCHA_PRIVATE_KEY'] = os.getenv('RECAPTCHA_PRIVATE_KEY')
 
 
-@app.route("/")
-def index():
-    api_data = json.loads(requests.get(
-        f"https://v2.convertapi.com/user?Secret={api_key}").text)
+@app.route("/", methods=['GET', 'POST'])
+def validation_test():
+    api_data = json.loads(requests.get(f"https://v2.convertapi.com/user?Secret={api_key}").text)
 
     seconds_left = api_data['SecondsLeft']
 
@@ -20,14 +25,16 @@ def index():
         pdf_available = False
     else:
         pdf_available = True
-    return render_template("index.html", pdf_available=pdf_available)
 
+    form = InputForm()
+    if form.validate_on_submit():
+        if form.docx.data:
+            filetype = "docx"
+        elif form.pdf.data:
+            filetype = "pdf"
 
-@app.route("/test_submit", methods=["POST"])
-def test_submit():
-    if request.method == 'POST':
-        filetype = request.form['filetype']
-        file = generate_document(request.form.to_dict(), filetype)
+        file = generate_document(form.data, filetype)
+
         try:
             return send_file(file, download_name=f"transgpguide.{filetype}")
         except file.getvalue() is None:
@@ -35,6 +42,8 @@ def test_submit():
                 return ("PDF downloads maxxed please download .docx version")
             else:
                 return ("An error occured, please try again later")
+
+    return render_template("index.html", form=form, pdf_available=pdf_available)
 
 
 if __name__ == '__main__':
