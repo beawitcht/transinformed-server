be82a0a14c8abb2abc07c042fad4817a4c2988c2
Bea <66849242+beawitcht@users.noreply.github.com>
2022-06-13 17:25:19 +0100

    adds referrer policy header

--- app/main.py
+++ app/main.py
@@ -33,6 +33,7 @@ def add_headers(response):
     response.headers['X-Frame-Options'] = 'DENY'
     response.headers['X-Content-Type-Options'] = 'nosniff'
     response.headers['X-XSS-Protection'] = '1; mode=block'
+    response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin'
     return response
 
 
