eb47af145fb87e143bed384826ff428e4280cb44
Bea <66849242+beawitcht@users.noreply.github.com>
2023-02-05 17:13:46 +0000

    implements blog hosting

---
+++ app/utilities.py
@@ -0,0 +1,26 @@
+import feedparser, re
+def prepare_blogs(url):
+    
+    feed = feedparser.parse(url)
+    entries = feed.entries
+    for item in entries:
+        tags=''
+        # remove queries from url
+        item.link = item.link.split('?')[0]
+        # add url title
+        item.url_title = item.title.replace(" ", "-")
+        
+        # format summary
+        if len(item.summary) > 150:
+            item.summary = item.summary[:150] + "..."
+        item.summary = re.sub("<[^>]*>", "",item.summary)
+        item.summary = item.summary.replace(u"\u00A0", " ")
+
+        # format tags       
+        for term in item.tags:
+            tags += term['term'] + ','
+        item.tags = tags[:-1]
+
+        
+    
+    return entries
