ec4a80244e64326bc176be32488444d0e6136d09
Bea <66849242+beawitcht@users.noreply.github.com>
2024-06-07 21:03:15 +0100

    adds sources, resouces, about + housekeeping

--- app/compilescss.bash
+++ app/compilescss.bash
@@ -2,3 +2,6 @@
 sass ./static/scss/base.scss:./static/css/base.css --style compressed
 sass ./static/scss/blog.scss:./static/css/blog.css --style compressed
 sass ./static/scss/blogs.scss:./static/css/blogs.css --style compressed
+sass ./static/scss/sources.scss:./static/css/sources.css --style compressed
+sass ./static/scss/resources.scss:./static/css/resources.css --style compressed
+sass ./static/scss/about.scss:./static/css/about.css --style compressed
