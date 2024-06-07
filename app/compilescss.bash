27cd5bc0fa844c1d0ab40143bba7e17bb7b5442f
Bea <66849242+beawitcht@users.noreply.github.com>
2024-06-07 15:32:14 +0100

    refactor and add blog mobile scss

--- app/compilescss.bash
+++ app/compilescss.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
+sass ./static/scss/base.scss:./static/css/base.css --style compressed
 sass ./static/scss/blog.scss:./static/css/blog.css --style compressed
 sass ./static/scss/blogs.scss:./static/css/blogs.css --style compressed
