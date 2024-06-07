21f6611bce2163c0145138c92bfe7962b582eff4
Bea <66849242+beawitcht@users.noreply.github.com>
2024-06-07 19:01:28 +0100

    add scroll to top + housekeeping

--- app/static/js/mobilenav.js
+++ app/static/js/mobilenav.js
@@ -1,22 +1,32 @@
-button = document.getElementById("hamburger")
-navMenu = document.getElementById("mobile")
+window.onscroll = function() {scrollDetected()};
 
-button.addEventListener("click", () =>{
-    const currentState = button.getAttribute("data-state");
-    if (!currentState || currentState === "closed") {
-        button.setAttribute("data-state", "opened");
-        button.setAttribute("aria-expanded", "true");
+button = document.getElementById("hamburger");
+navMenu = document.getElementById("mobile");
+backToTop = document.getElementById("backToTop");
 
-        navMenu.setAttribute("aria-expanded", "true");
-      } else {
-        button.setAttribute("data-state", "closed");
-        button.setAttribute("aria-expanded", "false");
-        
-        navMenu.setAttribute("aria-expanded", "false");
-      }
+button.addEventListener("click", () => {
+  const currentState = button.getAttribute("data-state");
+  if (!currentState || currentState === "closed") {
+    button.setAttribute("data-state", "opened");
+    button.setAttribute("aria-expanded", "true");
 
+    navMenu.setAttribute("aria-expanded", "true");
+  } else {
+    button.setAttribute("data-state", "closed");
+    button.setAttribute("aria-expanded", "false");
 
-
+    navMenu.setAttribute("aria-expanded", "false");
+  }
 });
 
+backToTop.addEventListener("click", () => {
+  document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
+});
 
+function scrollDetected() {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    document.getElementById("backToTop").style.display = "block";
+  } else {
+    document.getElementById("backToTop").style.display = "none";
+  }
+}
