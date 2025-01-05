9d8fd75c4189da21a493c7b280a88e648263c9e2
Bea <66849242+beawitcht@users.noreply.github.com>
2025-01-05 16:11:12 +0000

    deprioritise document generator

--- app/blueprints/blog_routes.py
+++ app/blueprints/blog_routes.py
@@ -8,10 +8,10 @@ entries = prepare_blogs("https://medium.com/feed/@transinformed")
 
 blog_bp = Blueprint('blog', __name__)
 
-@blog_bp.route("/blogs", methods=['GET'])
+@blog_bp.route("/", methods=['GET'])
 @cache.cached(timeout=60 * 60 * 24 * 7)
 def blogs():
-    return render_template("blogs.html", medium_feed=entries)
+    return render_template("index.html", medium_feed=entries)
 
 
 @blog_bp.route("/blogs/<string:title>", methods=['GET'])
